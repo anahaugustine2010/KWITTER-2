e10735882c2fd981709c08b4f6689f6c13c2898f
anahaugustine2010 <103948735+anahaugustine2010@users.noreply.github.com>
2022-08-17 20:43:40 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDlSQ5FqzMeXNyo-XDXcVciLG4c3tPZJDw",
+    authDomain: "kwitter-954bf.firebaseapp.com",
+    databaseURL: "https://kwitter-954bf-default-rtdb.firebaseio.com",
+    projectId: "kwitter-954bf",
+    storageBucket: "kwitter-954bf.appspot.com",
+    messagingSenderId: "933405889288",
+    appId: "1:933405889288:web:410c37375b755dbf3e12bd",
+    measurementId: "G-H5PKM5E9L3"
+  };
